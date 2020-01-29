908e5918c7d79cf56544f858876b21610c41bc9b
Anton Verburg <info@tec-V.nl>
2020-01-29 17:54:08 +0100

    Version 0.0.2: removed antonverburg from name, added dependency to smbus2, removed v0.0.1 as that was unusable

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="pi4ioe5v9xxxx-antonverburg", # Replace with your own username
+    name="pi4ioe5v9xxxx", 
     version="0.0.2",
     author="Anton Verburg",
     author_email="info@tec-V.nl",
@@ -13,9 +13,12 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/antonverburg/pi4ioe5v9xxxx",
     packages=setuptools.find_packages(),
+    install_requires=[
+        'smbus2',
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GPLv3+",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.7',
