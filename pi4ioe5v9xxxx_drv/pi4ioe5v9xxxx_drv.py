47518efdd583aebf76fb4c0736bfdda27e884977
Anton Verburg <info@tec-v.nl>
2023-08-25 09:25:09 +0200

    Renamed to pi4ioe5v9xxx_drv to prevent naming conflicts

--- pi4ioe5v9xxxx_drv/pi4ioe5v9xxxx_drv.py
+++ pi4ioe5v9xxxx_drv/pi4ioe5v9xxxx_drv.py
@@ -8,7 +8,8 @@ _PORT_VALUE = [0xFF]
 _READMODE = False
 _INVERT = False
 
-def setup(i2c_bus=1, i2c_addr = 0x20, bits=24, read_mode = False, invert = False):
+
+def setup(i2c_bus=1, i2c_addr=0x20, bits=24, read_mode=False, invert=False):
     """Set up the IO expander devices."""
     global _I2C_ADDR
     global _BITS
@@ -24,7 +25,7 @@ def setup(i2c_bus=1, i2c_addr = 0x20, bits=24, read_mode = False, invert = False
     while bits % 8:
         bits += 1
     # Increase array size
-    _PORT_VALUE = [0xFF]* int(bits / 8)
+    _PORT_VALUE = [0xFF] * int(bits / 8)
     # Set up I2C bus connectivity
     _BUS = SMBus(i2c_bus)
     # Write 1 to all pins to prepaire them for reading, or bring hardware in a defined state
@@ -32,10 +33,11 @@ def setup(i2c_bus=1, i2c_addr = 0x20, bits=24, read_mode = False, invert = False
     if _BUS:
         _BUS.i2c_rdwr(msg)
     else:
-       raise ReferenceError("I2C bus was not created, please check I2C address!")
+        raise ReferenceError("I2C bus was not created, please check I2C address!")
     # If in read mode: do first hw read to have memory ready
     if read_mode:
         hw_to_memory()
+
 
 def hw_to_memory():
     """Read pin data from hardware and write to memory."""
@@ -43,7 +45,7 @@ def hw_to_memory():
     global _PORT_VALUE
     global _I2C_ADDR
     global _READMODE
-    if(not _READMODE):
+    if not _READMODE:
         raise ValueError("You cannot read from the hardware as it is in write mode!")
     msg = i2c_msg.read(_I2C_ADDR, len(_PORT_VALUE))
     if _BUS:
@@ -51,7 +53,7 @@ def hw_to_memory():
         for k in range(msg.len):
             _PORT_VALUE[k] = int(msg.buf[k].hex(), 16)
     else:
-         raise ReferenceError("I2C bus was not created, did you run setup?")
+        raise ReferenceError("I2C bus was not created, did you run setup?")
 
 
 def memory_to_hw():
@@ -59,7 +61,7 @@ def memory_to_hw():
     global _BUS
     global _PORT_VALUE
     global _I2C_ADDR
-    if(_READMODE):
+    if _READMODE:
         raise ValueError("You cannot write to hardware as it is in read mode!")
     msg = i2c_msg.write(_I2C_ADDR, _PORT_VALUE)
     if _BUS:
@@ -67,13 +69,14 @@ def memory_to_hw():
     else:
         raise ValueError("I2C bus not available, did you run setup?")
 
-def pin_from_memory(pin:int):
+
+def pin_from_memory(pin: int):
     """Read pin value from memory and return its value"""
     global _BITS
     global _READMODE
     global _PORT_VALUE
     global _INVERT
-    if(pin>_BITS):
+    if pin > _BITS:
         raise ValueError("Pin number is larger than number of bits available!")
     byte_nr = int((pin - 1) / 8)
     bit_nr = int((pin - 1) - (byte_nr * 8))
@@ -84,11 +87,11 @@ def pin_from_memory(pin:int):
         return _INVERT
 
 
-def pin_to_memory(pin:int, value:bool):
+def pin_to_memory(pin: int, value: bool):
     """Write pin value to memory"""
     global _PORT_VALUE
     global _BITS
-    if(pin>_BITS):
+    if pin > _BITS:
         raise ValueError("Pin number is larger than number of bits available!")
     local_value = value
     if _INVERT:
@@ -103,6 +106,7 @@ def pin_to_memory(pin:int, value:bool):
         if local_value:
             _PORT_VALUE[byte_nr] |= mask
     else:
-        raise ValueError ( "Writing pin %d to  bit %d of port %d while IO-expander has only %d ports (%d bits)!" % (pin, bit_nr, byte_nr, len(_PORT_VALUE), len(PORT_VALUE)*8))
-
-
+        raise ValueError(
+            "Writing pin %d to  bit %d of port %d while IO-expander has only %d ports (%d bits)!"
+            % (pin, bit_nr, byte_nr, len(_PORT_VALUE), len(PORT_VALUE) * 8)
+        )
